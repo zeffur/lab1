1ed5ce41445de15bf14dcb4c59268f5298ccd323
Anastasia <zeffurs@gmail.com>
2021-09-03 22:48:38 +0700

    Overflow case solved

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -17,22 +17,30 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onClick(View view)
     {
-        // выводим сообщение
         EditText et1 = findViewById(R.id.editText1);
         EditText et2 = findViewById(R.id.editText2);
         TextView tv1 = findViewById(R.id.tv1);
         TextView tv2 = findViewById(R.id.tv2);
         if(et1.getText().length() != 0 && et2.getText().length() != 0)
         {
-            String value = et1.getText().toString();
-            int et1v = Integer.parseInt(value);
-            value = et2.getText().toString();
-            int et2v = Integer.parseInt(value);
-            int a = Min.min(et1v,et2v);
-            int b = Max.max(et1v,et2v);
+            String value1 = et1.getText().toString();
+            String value2 = et2.getText().toString();
+            long et1v = Long.parseLong(value1);
+            long et2v = Long.parseLong(value2);
+            if (et1v <= Integer.MAX_VALUE && et1v >= Integer.MIN_VALUE && et2v <= Integer.MAX_VALUE && et2v >= Integer.MIN_VALUE)
+            {
+                int one = (int)et1v;
+                int two = (int)et2v;
+                int a = Min.min(one,two);
+                int b = Max.max(one,two);
 
-            tv1.setText(String.valueOf(a));
-            tv2.setText(String.valueOf(b));
+                tv1.setText(String.valueOf(a));
+                tv2.setText(String.valueOf(b));
+            }
+            else
+            {
+                Toast.makeText(this, "Введенные числа слишком велики", Toast.LENGTH_SHORT).show();
+            }
         }
         else
         {
